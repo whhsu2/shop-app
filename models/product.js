4365af477a29a8b8e526fb85a86590eee650b012
morris754 <morris754@gmail.com>
2020-07-27 22:21:26 -0400

    add controller and models folder

---
+++ models/product.js
@@ -0,0 +1,37 @@
+const fs = require('fs')
+const path = require('path')
+const products = []
+
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'products.json'
+)
+
+const getProductsFromFile = callback => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            callback([])
+        }
+        callback(JSON.parse(fileContent)) 
+    })
+}
+
+module.exports = class Product { 
+    constructor(title) {
+        this.title = title
+    }
+
+    save() {
+        getProductsFromFile( products => {
+            products.push(this)
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err)
+            })
+        })
+    }
+
+    static fetchAll(callback) {
+        getProductsFromFile(callback)
+    }
+}
